01a88d9ccb894b571d2f41a93b672c454884d859
V. Akansha <85068589+unknownuser2329@users.noreply.github.com>
2022-09-03 08:36:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Favicon from "react-favicon"; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Favicon url="https://www.linkpicture.com/q/icons8-news-96.png"></Favicon>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
